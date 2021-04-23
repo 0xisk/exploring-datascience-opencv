daf9c2a39489e45103cd135290c7bdd7f1accb49
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-23 21:22:20 +0200

    Section\ \#1\ -\ Basics/read.py: Implementing a Simple read for large image

--- Section #1 - Basics/read.py
+++ Section #1 - Basics/read.py
@@ -2,6 +2,9 @@ import cv2 as cv
 
 img = cv.imread("../Resources/Photos/cat.jpg")
 
-cv.imshow('Cat', img)
+img_large = cv.imread("../Resources/Photos/cat_large.jpg")
+
+# cv.imshow('Cat', img)
+cv.imshow('Cat', img_large)
 
 cv.waitKey(0)
