6c89ec87c41db63c131cdd3dde341b69179d8e45
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-27 07:01:38 +0200

    Section\#1-Basics/resize_rescale.py: Testing resizing and rescaling for Videos

--- Section#1-Basics/resize_rescale.py
+++ Section#1-Basics/resize_rescale.py
@@ -1,10 +1,10 @@
 import cv2 as cv 
 
-img = cv.imread('./Resources/Photos/cat_large.jpg')
-cv.imshow('Cat', img)
+# img = cv.imread('./Resources/Photos/cat_large.jpg')
+# cv.imshow('Cat', img)
 
 
-def rescaleFrame(frame, scale=0.75):
+def rescale_frame(frame, scale=0.75):
     # frame.shape[1] = frame width
     width = int(frame.shape[1] * scale)
     # frame.shape[0] = frame height
@@ -15,4 +15,25 @@ def rescaleFrame(frame, scale=0.75):
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 
-cv.waitKey(0)
+# Reading Videos
+# You can use int 0 for referencing to webcam
+capture = cv.VideoCapture('./Resources/Videos/dog.mp4')
+
+# Reading a video using a while loop to read the video frame by frame
+while True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescale_frame(frame, scale=.2)
+
+    # To show the video frame by frame
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    # To stop the video from running infinitely, if the letter 'd' is pressed
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+
+cv.destroyAllWindows()
+
