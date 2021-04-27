387ebd151079b8c2b21856c1110703f0aefb84e3
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-27 07:45:46 +0200

    Section\#1-Basics/draw.py: Drawing a beatiful circle

--- Section#1-Basics/draw.py
+++ Section#1-Basics/draw.py
@@ -29,4 +29,11 @@ cv.imshow('Filled Rectangle Blank', blank)
 cv.rectangle(blank, (0, 0), (blank.shape[1]//2, blank.shape[0]//2), (0, 255, 0), thickness=-1)
 cv.imshow('Filled Rectangle Blank with black.shape dimensions', blank)
 
+# 3. Draw a Circle
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0, 0, 255), thickness=3)
+cv.imshow('Circle Blank', blank)
+
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0, 0, 255), thickness=-1)
+cv.imshow('Thick Circle Blank', blank)
+
 cv.waitKey(0)
