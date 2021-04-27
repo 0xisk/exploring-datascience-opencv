d3f287670fa3611438d87c321aa7a25856f43db2
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-27 07:33:30 +0200

    Section\#1-Basics/draw.py: Colouring the image with Red and Green, by colouring out the pixels 3D

--- Section#1-Basics/draw.py
+++ Section#1-Basics/draw.py
@@ -2,7 +2,14 @@ import cv2 as cv
 import numpy as np
 
 # Working on a Blank Image
-blank = np.zeros((500,500), dtype='uint8')
+blank = np.zeros((500, 500, 3), dtype='uint8')
 cv.imshow('Blank', blank)
+
+# 1. Point the image a certain colour
+blank[:] = 0, 255, 0
+cv.imshow('Green Blank', blank)
+
+blank[:] = 0, 0, 255
+cv.imshow('Red Blank', blank)
 
 cv.waitKey(0)
