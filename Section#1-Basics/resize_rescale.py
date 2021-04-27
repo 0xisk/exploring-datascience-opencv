e9576890933ce7f4deba543427c3633f223cc2ca
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-27 06:50:38 +0200

    Section\#1-Basics/resize_rescale.py: Implmenting a function for rescaling

--- Section#1-Basics/resize_rescale.py
+++ Section#1-Basics/resize_rescale.py
@@ -3,4 +3,16 @@ import cv2 as cv
 img = cv.imread('./Resources/Photos/cat_large.jpg')
 cv.imshow('Cat', img)
 
+
+def rescaleFrame(frame, scale=0.75):
+    # frame.shape[1] = frame width
+    width = int(frame.shape[1] * scale)
+    # frame.shape[0] = frame height
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
 cv.waitKey(0)
