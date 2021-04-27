dee7e49fd5a9e2f71f412272a516bd2ce8f6ae11
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-27 07:48:43 +0200

    Section\#1-Basics/draw.py: Drwaing a line

--- Section#1-Basics/draw.py
+++ Section#1-Basics/draw.py
@@ -33,7 +33,11 @@ cv.imshow('Filled Rectangle Blank with black.shape dimensions', blank)
 cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0, 0, 255), thickness=3)
 cv.imshow('Circle Blank', blank)
 
-cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0, 0, 255), thickness=-1)
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (255, 255, 0), thickness=-1)
 cv.imshow('Thick Circle Blank', blank)
+
+# 4. Draw a line
+cv.line(blank, (0, 0), (blank.shape[1]//2, blank.shape[0]//2), (255, 255, 255), thickness=3)
+cv.imshow('Line Blank', blank)
 
 cv.waitKey(0)
