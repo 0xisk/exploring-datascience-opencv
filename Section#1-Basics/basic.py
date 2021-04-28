fe0c19384462f85cad16103a97bc8d9d50cbe586
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-28 06:43:22 +0200

    Section\#1-Basics/basic.py: Reduce the amount of Edges by blurring first

--- Section#1-Basics/basic.py
+++ Section#1-Basics/basic.py
@@ -8,15 +8,20 @@ gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 cv.imshow('Gray Image', gray)
 
 # Converting to Blur image
-blurImg = cv.blur(img, (50, 50))
-cv.imshow('blurred image', blurImg)
+blur_img = cv.blur(img, (50, 50))
+cv.imshow('blurred image', blur_img)
 
 # Converting to Gaussian Blur
-gaussianBlur = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
-cv.imshow('Blur', gaussianBlur)
+gaussian_blur = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
+cv.imshow('Blur', gaussian_blur)
 
 # Edge Cascade
 canny = cv.Canny(img, 125, 175)
 cv.imshow('Canny Edges', canny)
+
+# Reduce the amount of edges by passing a blur image
+canny_blur = cv.Canny(gaussian_blur, 125, 175)
+cv.imshow('Canny Edges', canny_blur)
+
 
 cv.waitKey(0)
