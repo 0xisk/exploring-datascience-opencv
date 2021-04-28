4ec1a5d050cfffb329917e5b579daff1bd0e7e81
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-28 06:30:48 +0200

    Section\#1-Basics/basic.py: Implmenting the convertion to blur image

--- Section#1-Basics/basic.py
+++ Section#1-Basics/basic.py
@@ -7,4 +7,8 @@ cv.imshow('0003.jpeg', img)
 gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 cv.imshow('Gray Image', gray)
 
+# Converting to Blur image
+blurImg = cv.blur(img, (50, 50))
+cv.imshow('blurred image', blurImg)
+
 cv.waitKey(0)
