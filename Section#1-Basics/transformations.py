3ef336a923635304d7e0a97d4392349ec8b9c322
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-30 16:22:53 +0200

    Section\#1-Basics/transformations.py: Implementing the translate function for shifting the image dimennsions

---
+++ Section#1-Basics/transformations.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("./Resources/Photos/0003.jpeg")
+
+cv.imshow('Weirdo', img)
+
+# Translation
+def translate(img, x, y):
+    trans_mat = np.float32([[1, 0, x], [0, 1, y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, trans_mat, dimensions)
+
+# -x --> left
+# -y --> up
+# x --> right
+# y --> down
+
+translated = translate(img, 100, 100)
+cv.imshow('Translated', translated)
+
+cv.waitKey(0)
+
