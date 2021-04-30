16a9b93b5520a33eb8a795f3539e8627ac042d58
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-30 17:03:41 +0200

    Section\#1-Basics/contour.py: Chaning to blur img reduces the number of contours Changing to CHAIN_APPROX_SIMPLE has the same effect of CHAIN_APPROX_NONE

--- Section#1-Basics/contour.py
+++ Section#1-Basics/contour.py
@@ -8,10 +8,16 @@ cv.imshow('Weirdo', img)
 gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 cv.imshow('Gray', gray)
 
-canny = cv.Canny(img, 125, 175)
+blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+canny = cv.Canny(blur, 125, 175)
 cv.imshow('Canny Edges', canny)
 
-contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+# contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+# print(f'{len(contours)} contour(s) found!')
+
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contour(s) found!')
 
 cv.waitKey(0)
