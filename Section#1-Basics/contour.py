05dee868b96ee3d8eace6d7588b27fb15d852008
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-30 17:18:22 +0200

    Section\#1-Basics/contour.py: Drawing a contours with a black image

--- Section#1-Basics/contour.py
+++ Section#1-Basics/contour.py
@@ -2,11 +2,13 @@ import cv2 as cv
 import numpy as np
 
 img = cv.imread("./Resources/Photos/0003.jpeg")
-
 cv.imshow('Weirdo', img)
 
+blank = np.zeros(img.shape, dtype='uint8')
+cv.imshow('Blank', blank)
+
 gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-cv.imshow('Gray', gray)
+# cv.imshow('Gray', gray)
 
 # blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
 # cv.imshow('Blur', blur)
@@ -18,9 +20,12 @@ cv.imshow('Gray', gray)
 # print(f'{len(contours)} contour(s) found!')
 
 ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
-cv.imshow('Thresh', thresh)
+# cv.imshow('Thresh', thresh)
 
 contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contour(s) found!')
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 1)
+cv.imshow('Contours Draws', blank)
 
 cv.waitKey(0)
