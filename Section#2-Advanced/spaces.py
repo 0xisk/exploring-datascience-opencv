7ba2feccaceb72b13b2ccb8a8376d6cfa9cd701f
iskanderandrews <iskander.s.andrews@gmail.com>
2021-04-30 17:37:37 +0200

    Section\#2-Advanced/spaces.py: Converting from RGR to RGB

--- Section#2-Advanced/spaces.py
+++ Section#2-Advanced/spaces.py
@@ -1,8 +1,12 @@
 import cv2 as cv
 import numpy as np
+# import matplotlib.pyplot as plt
 
 img = cv.imread("./Resources/Photos/0003.jpeg")
 cv.imshow('Weirdo', img)
+
+# plt.imshow(img)
+# plt.show()
 
 # BGR to Grayscale
 gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
@@ -15,5 +19,9 @@ cv.imshow('HSV', hsv)
 # BGR to L*a*b
 lab = cv.cvtColor(img, cv.COLOR_RGB2LAB)
 cv.imshow('LAB', lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB', rgb)
 
 cv.waitKey(0)
