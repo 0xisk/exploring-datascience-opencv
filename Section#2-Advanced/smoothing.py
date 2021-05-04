4d2f8701f66cd2724f112b3dc49f0863524ed12b
iskanderandrews <iskander.s.andrews@gmail.com>
2021-05-04 22:56:48 +0200

    Section\#2-Advanced/smoothing.py: Implementing the Gaussian Blur

--- Section#2-Advanced/smoothing.py
+++ Section#2-Advanced/smoothing.py
@@ -3,8 +3,12 @@ import cv2 as cv
 img = cv.imread("./Resources/Photos/0003.jpeg")
 cv.imshow('Weirdo', img)
 
-# Averaging
+# Averaging Blur
 average = cv.blur(img, (7, 7))
 cv.imshow('Average Blur', average)
+
+# Gaussian Blur
+gauss = cv.GaussianBlur(img, (7, 7), 0)
+cv.imshow('Gaussian Blur', gauss)
 
 cv.waitKey(0)
