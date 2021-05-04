81a1bc918f97241f6a6d473c813f91fd106625dd
iskanderandrews <iskander.s.andrews@gmail.com>
2021-05-04 22:54:16 +0200

    Section\#2-Advanced/smoothing.py: IMplementing the first testing to the Average blurring

--- Section#2-Advanced/smoothing.py
+++ Section#2-Advanced/smoothing.py
@@ -3,4 +3,8 @@ import cv2 as cv
 img = cv.imread("./Resources/Photos/0003.jpeg")
 cv.imshow('Weirdo', img)
 
+# Averaging
+average = cv.blur(img, (7, 7))
+cv.imshow('Average Blur', average)
+
 cv.waitKey(0)
