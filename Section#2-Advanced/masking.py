55801b1ff65791f121326f7265ecb9bf40dd8c56
iskanderandrews <iskander.s.andrews@gmail.com>
2021-05-07 21:43:03 +0200

    Section\#2-Advanced/masking.py: Implmenting a weird masking

--- Section#2-Advanced/masking.py
+++ Section#2-Advanced/masking.py
@@ -13,4 +13,13 @@ cv.imshow("Mask", mask)
 masked = cv.bitwise_and(img, img, mask=mask)
 cv.imshow("Masked Image", masked)
 
+rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
+circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1)
+
+weird_shape = cv.bitwise_and(circle, rectangle)
+cv.imshow("Weird Shape", weird_shape)
+
+masked_weird = cv.bitwise_and(img, img, mask=weird_shape)
+cv.imshow("Weird Image", masked_weird)
+
 cv.waitKey(0)
